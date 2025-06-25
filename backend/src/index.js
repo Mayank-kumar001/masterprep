af4fdc590f1c3c337253f78e9653b2c34d8cb892
Mayank-kumar001 <dev.01mayank@gmail.com>
2025-06-25 09:24:40 +0530

    add sample enviroment varialble file

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://ec2-3-108-235-156.ap-south-1.compute.amazonaws.com:8080',
+    origin: 'http://localhost:5173',
     credentials: true,
     // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     // allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
